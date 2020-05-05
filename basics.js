4739dd4a4b2bb3d4a108d25e52d115ab5a0972a6
kamalvemula <63295080+kamalvemula@users.noreply.github.com>
2020-05-05 10:26:15 +0530

    Add files via upload

---
+++ basics.js
@@ -0,0 +1,5 @@
+const basics =() =>{
+    console.log("This is a basic function using the defined package");
+}
+
+module.exports = basics;
---
+++ index.js
@@ -0,0 +1,3 @@
+const basics = require('./basics');
+
+module.exports = basics;
